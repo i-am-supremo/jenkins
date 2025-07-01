2fa72b0b22c4839b43a1750cc38d3e48a583d67e
Manish <121449948+i-am-supremo@users.noreply.github.com>
2025-07-01 19:19:42 +0530

    Update TestService.java

--- src/main/java/com/learning/jenkins/TestService.java
+++ src/main/java/com/learning/jenkins/TestService.java
@@ -7,6 +7,6 @@ public class TestService {
 
 
     public String returnTestData() {
-        return "Hello User Getting Data From Service";
+        return "Hello User Getting Data From Services";
     }
 }
